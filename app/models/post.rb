6cf66f4d64dbfbb92c28a8c5c84061eeb882e3b4
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-19 21:00:02 -0400

    commit tree 5a5d528daad9edb1507782d41c72f02abd065e6e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+	validate :cannot_be_true_facts
+
+	def cannot_be_true_facts
+		if self.title == "True Facts"
+			errors.add(:title, "That title is invalid")
+		end
+	end
 end
